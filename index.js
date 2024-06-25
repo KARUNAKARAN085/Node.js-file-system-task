c0cd709410e723bf8de9c1e8faca1d67cc345817
KARUNAKARAN085 <karunakaranrk2001rk2001@gmail.com>
2024-06-25 15:28:45 +0530

    final commit

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const { createServer } = require('http');
+const express = require('express');
+const fs = require('fs');
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
-});
+const app = express();
+app.use(express.json());
+
+//generate file name as created date and time
+let date = new Date();
+let filename = date.getFullYear() + '-' + (date.getMonth() + 1) + '-'+ date.getDate() + ' - ' + date.getHours() + '.' + date.getMinutes() + '.'+ date.getSeconds() + `.txt`;
+function generatefile(filename){
+    return filename;
+}
+
+app.get( '/date/', (req, res) => {
+    const code = req.body.code;
+    
+    fs.writeFile(`./createdfiles/` + generatefile(filename),code,(err)=>{
+        if(!err){
+            res.send('File created');
+        } else{
+            res.send(err.message);
+        }
+    })
+})
+
+//reading the files from folder
+app.get('/files', (req, res) => {
+    fs.readdir('./createdfiles/', (err, files) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            res.send(files);
+        }
+    })
+})
 
 // starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
